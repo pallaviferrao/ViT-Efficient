71ee99cf0d92bdee4b585ff2021e7a3d98b7c2ca
Pallavi <pallaviferrao2004@gmail.com>
2025-03-11 11:18:16 -0400

    utils change

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import json, os, math
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.nn import functional as F
@@ -59,80 +58,80 @@ def load_experiment(experiment_name, checkpoint_name="model_final.pt", base_dir=
     return config, model, train_losses, test_losses, accuracies
 
 
-def visualize_images():
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True)
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    # Pick 30 samples randomly
-    indices = torch.randperm(len(trainset))[:30]
-    images = [np.asarray(trainset[i][0]) for i in indices]
-    labels = [trainset[i][1] for i in indices]
-    # Visualize the images using matplotlib
-    fig = plt.figure(figsize=(10, 10))
-    for i in range(30):
-        ax = fig.add_subplot(6, 5, i + 1, xticks=[], yticks=[])
-        ax.imshow(images[i])
-        ax.set_title(classes[labels[i]])
+# def visualize_images():
+#     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+#                                             download=True)
+#     classes = ('plane', 'car', 'bird', 'cat',
+#                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+#     # Pick 30 samples randomly
+#     indices = torch.randperm(len(trainset))[:30]
+#     images = [np.asarray(trainset[i][0]) for i in indices]
+#     labels = [trainset[i][1] for i in indices]
+#     # Visualize the images using matplotlib
+#     fig = plt.figure(figsize=(10, 10))
+#     for i in range(30):
+#         ax = fig.add_subplot(6, 5, i + 1, xticks=[], yticks=[])
+#         ax.imshow(images[i])
+#         ax.set_title(classes[labels[i]])
 
 
-@torch.no_grad()
-def visualize_attention(model, output=None, device="cuda"):
-    """
-    Visualize the attention maps of the first 4 images.
-    """
-    model.eval()
-    # Load random images
-    num_images = 30
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True)
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    # Pick 30 samples randomly
-    indices = torch.randperm(len(testset))[:num_images]
-    raw_images = [np.asarray(testset[i][0]) for i in indices]
-    labels = [testset[i][1] for i in indices]
-    # Convert the images to tensors
-    test_transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Resize((32, 32)),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    images = torch.stack([test_transform(image) for image in raw_images])
-    # Move the images to the device
-    images = images.to(device)
-    model = model.to(device)
-    # Get the attention maps from the last block
-    logits, attention_maps = model(images, output_attentions=True)
-    # Get the predictions
-    predictions = torch.argmax(logits, dim=1)
-    # Concatenate the attention maps from all blocks
-    attention_maps = torch.cat(attention_maps, dim=1)
-    # select only the attention maps of the CLS token
-    attention_maps = attention_maps[:, :, 0, 1:]
-    # Then average the attention maps of the CLS token over all the heads
-    attention_maps = attention_maps.mean(dim=1)
-    # Reshape the attention maps to a square
-    num_patches = attention_maps.size(-1)
-    size = int(math.sqrt(num_patches))
-    attention_maps = attention_maps.view(-1, size, size)
-    # Resize the map to the size of the image
-    attention_maps = attention_maps.unsqueeze(1)
-    attention_maps = F.interpolate(attention_maps, size=(32, 32), mode='bilinear', align_corners=False)
-    attention_maps = attention_maps.squeeze(1)
-    # Plot the images and the attention maps
-    fig = plt.figure(figsize=(20, 10))
-    mask = np.concatenate([np.ones((32, 32)), np.zeros((32, 32))], axis=1)
-    for i in range(num_images):
-        ax = fig.add_subplot(6, 5, i + 1, xticks=[], yticks=[])
-        img = np.concatenate((raw_images[i], raw_images[i]), axis=1)
-        ax.imshow(img)
-        # Mask out the attention map of the left image
-        extended_attention_map = np.concatenate((np.zeros((32, 32)), attention_maps[i].cpu()), axis=1)
-        extended_attention_map = np.ma.masked_where(mask == 1, extended_attention_map)
-        ax.imshow(extended_attention_map, alpha=0.5, cmap='jet')
-        # Show the ground truth and the prediction
-        gt = classes[labels[i]]
-        pred = classes[predictions[i]]
-        ax.set_title(f"gt: {gt} / pred: {pred}", color=("green" if gt == pred else "red"))
-    if output is not None:
-        plt.savefig(output)
-    plt.show()
+# @torch.no_grad()
+# def visualize_attention(model, output=None, device="cuda"):
+#     """
+#     Visualize the attention maps of the first 4 images.
+#     """
+#     model.eval()
+#     # Load random images
+#     num_images = 30
+#     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True)
+#     classes = ('plane', 'car', 'bird', 'cat',
+#                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+#     # Pick 30 samples randomly
+#     indices = torch.randperm(len(testset))[:num_images]
+#     raw_images = [np.asarray(testset[i][0]) for i in indices]
+#     labels = [testset[i][1] for i in indices]
+#     # Convert the images to tensors
+#     test_transform = transforms.Compose(
+#         [transforms.ToTensor(),
+#          transforms.Resize((32, 32)),
+#          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+#     images = torch.stack([test_transform(image) for image in raw_images])
+#     # Move the images to the device
+#     images = images.to(device)
+#     model = model.to(device)
+#     # Get the attention maps from the last block
+#     logits, attention_maps = model(images, output_attentions=True)
+#     # Get the predictions
+#     predictions = torch.argmax(logits, dim=1)
+#     # Concatenate the attention maps from all blocks
+#     attention_maps = torch.cat(attention_maps, dim=1)
+#     # select only the attention maps of the CLS token
+#     attention_maps = attention_maps[:, :, 0, 1:]
+#     # Then average the attention maps of the CLS token over all the heads
+#     attention_maps = attention_maps.mean(dim=1)
+#     # Reshape the attention maps to a square
+#     num_patches = attention_maps.size(-1)
+#     size = int(math.sqrt(num_patches))
+#     attention_maps = attention_maps.view(-1, size, size)
+#     # Resize the map to the size of the image
+#     attention_maps = attention_maps.unsqueeze(1)
+#     attention_maps = F.interpolate(attention_maps, size=(32, 32), mode='bilinear', align_corners=False)
+#     attention_maps = attention_maps.squeeze(1)
+#     # Plot the images and the attention maps
+#     fig = plt.figure(figsize=(20, 10))
+#     mask = np.concatenate([np.ones((32, 32)), np.zeros((32, 32))], axis=1)
+#     for i in range(num_images):
+#         ax = fig.add_subplot(6, 5, i + 1, xticks=[], yticks=[])
+#         img = np.concatenate((raw_images[i], raw_images[i]), axis=1)
+#         ax.imshow(img)
+#         # Mask out the attention map of the left image
+#         extended_attention_map = np.concatenate((np.zeros((32, 32)), attention_maps[i].cpu()), axis=1)
+#         extended_attention_map = np.ma.masked_where(mask == 1, extended_attention_map)
+#         ax.imshow(extended_attention_map, alpha=0.5, cmap='jet')
+#         # Show the ground truth and the prediction
+#         gt = classes[labels[i]]
+#         pred = classes[predictions[i]]
+#         ax.set_title(f"gt: {gt} / pred: {pred}", color=("green" if gt == pred else "red"))
+#     if output is not None:
+#         plt.savefig(output)
+#     plt.show()
